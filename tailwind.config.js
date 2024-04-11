4648ad295e50942a53e40585dbf25908aa3eaa79
Argenis Rodriguez <estefreric@gmail.com>
2024-04-11 01:16:24 -0400

    fix bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: ["class"],
   content: ["./*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
